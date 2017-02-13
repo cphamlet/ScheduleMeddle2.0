e312c93b9efa1c1342d3d858206b8241985e6b8f
root <root@raspberrypi.(none)>
2017-02-13 18:32:25 -0500

    added beta version of comp110

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Schedule Meddle requires some dependencies. They are:
 
 Java
 PHP-5
+
+Important! Change owner of bin and uploads directory to 
+www-data. Then chmod 700 the uploads directory so that others do not execute php scripts
--- comp110beta/getSched.php
+++ comp110beta/getSched.php
@@ -1,18 +1,15 @@
 <?php
 $onyen =  htmlspecialchars($_POST["onyen"]);
-
-
-
 // Get cURL resource
 $curl = curl_init();
+
+echo "right";
 // Set some options - we are passing in a useragent too here
 curl_setopt($curl,CURLOPT_USERAGENT, 'cphamlet');
 //gives the access token to the GitHub API for authorization
- curl_setopt($curl,CURLOPT_URL,'https://api.github.com/repos/comp110/KarenBot/contents/data/spring-17/staff/'.$onyen.'.csv?access_token=');
+ curl_setopt($curl,CURLOPT_URL,'https://api.github.com/repos/comp110/KarenBot/contents/data/spring-17/staff/'.$onyen.'.csv?access_token=72bb9ee6c66545e82644e5cfb3e977c59c8be365');
  //return curl_exec value as a string instead of output
  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-
-
 
 curl_setopt($curl,CURLOPT_CONNECTTIMEOUT,8);
 curl_setopt($curl,CURLOPT_TIMEOUT,8);
@@ -28,9 +25,9 @@ $response = $response->{'download_url'};
 $curl = curl_init();
 // Set some options - we are passing in a useragent too here
 curl_setopt($curl,CURLOPT_USERAGENT, 'cphamlet');
-//curl_setopt($curl,CURLOPT_HTTPHEADER, 'Authorization: token
+//curl_setopt($curl,CURLOPT_HTTPHEADER, 'Authorization: token 435e158a0a980c180ea2b53621d6bf5077da6147');
 
-//url looks something like: https://raw.githubusercontent.com/comp110/KarenBot/master/data/spring-17/staff/atsirk.csv?token=asjbajbefajbfjsbdfjJHBF, but isn't because the token changes 
+//url looks something like: https://raw.githubusercontent.com/comp110/KarenBot/master/data/spring-17/staff/atsirk.csv?token=AGfgEirFtj1H5fg-ldyG99YZ-5EtAZcNks5Ym8x6wA%3D%3D, but isn't because the token changes 
 
 curl_setopt($curl,CURLOPT_URL,$response);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
